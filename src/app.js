546df733855148edbfb67e8be310e3b762525024
Chris Wilkening <clw13clw13@gmail.com>
2016-12-15 11:27:37 -0500

    fixed start button and footer position

---
+++ src/app.js
@@ -0,0 +1,24 @@
+class Simon {
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+$('#b0').on('click', carryOutFunction);
+$('#b1').on('click', carryOutFunction);
+$('#b2').on('click', carryOutFunction);
+$('#b3').on('click', carryOutFunction);
