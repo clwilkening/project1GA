dbbfe1f3e62290d6963e26f0454213e75b3c00da
Chris Wilkening <clw13clw13@gmail.com>
2016-12-18 20:32:47 -0500

    added styling and new sounds

--- src/app.js
+++ src/app.js
@@ -21,7 +21,7 @@ let currentLevel = 1;
     $('.b0').addClass('b0-red');
     setTimeout(function(){
     $('.b0').removeClass('b0-red');
-    }, 200);
+    }, 300);
     document.getElementById('playb0').play();
   }
 
@@ -29,7 +29,7 @@ let currentLevel = 1;
     $('.b1').addClass('b1-yellow');
     setTimeout(function(){
     $('.b1').removeClass('b1-yellow');
-    }, 200);
+    }, 300);
     document.getElementById('playb1').play();
   }
 
@@ -37,7 +37,7 @@ let currentLevel = 1;
     $('.b2').addClass('b2-green');
     setTimeout(function(){
     $('.b2').removeClass('b2-green');
-    }, 200);
+    }, 300);
     document.getElementById('playb2').play();
   }
 
@@ -45,10 +45,9 @@ let currentLevel = 1;
     $('.b3').addClass('b3-blue');
     setTimeout(function(){
     $('.b3').removeClass('b3-blue');
-    }, 200);
+    }, 300);
     document.getElementById('playb3').play();
   }
-
 
 //this buttonClick grabbs the buttonID from simonShow.
 //It checks too match the passed ID with the correct color.
@@ -56,6 +55,18 @@ let currentLevel = 1;
 //  on the screen.
   function buttonClick(buttonID) {
     let color;
+      if (isFirstMove === true) {
+        if (buttonID === 'b0') {
+          color = 'red';
+        } else if (buttonID === 'b1') {
+          color = 'yellow';
+        } else if (buttonID === 'b2') {
+          color = 'green';
+        } else if (buttonID === 'b3') {
+          color = 'blue';
+        }
+        isFirstMove = false;
+      } else {
       if (buttonID === 'b0') {
         color = 'red';
         document.getElementById('playb0').play();
@@ -69,10 +80,11 @@ let currentLevel = 1;
         color = 'blue';
         document.getElementById('playb3').play();
       };
+    };
     $(`.${buttonID}`).addClass(`${buttonID}-${color}`);
     setTimeout(function() {
     $(`.${buttonID}`).removeClass(`${buttonID}-${color}`);
-    }, 300);
+    }, 400);
   };
 
   function simonShow() {
@@ -87,7 +99,7 @@ let currentLevel = 1;
           console.log(simonCurrentArray);
       //call buttonClick to automate the current array with the new move
         buttonClick(simonCurrentArray[i]);
-        }, 520 * x);
+        }, 500 * x);
         x++
 
         // setTimeout(function() {
@@ -106,6 +118,7 @@ let currentLevel = 1;
     //if (playerTurn === false) {
       simonCurrentArray.push(simonButtonOptions[(Math.floor(Math.random()*4))]);
       simonShow();
+      $('.count').html(currentLevel);
     };
     //playerTurn = true;
     console.log(playerTurn);
@@ -134,16 +147,17 @@ function nextLevel() {
             if(currentLevel === 5) {
               alert('Great YOU WIN! Click start to play again.');
             } else {
-              setTimeout(function() {
-                alert('Next Level!');
-              }, 350);
+              // setTimeout(function() {
+              //   alert('Next Level!');
+              // }, 400);
+              //no longer needed
               setTimeout(function() {
                 nextLevel();
-              }, 400);
+              }, 800);
               playerTurn = false;
               setTimeout(function() {
                 simonNewMove();
-              }, 500);
+              }, 1000);
             }
           }
         }
@@ -177,6 +191,7 @@ function newGame() {
   playerCurrentArray = [];
   currentLevel = 1;
   playerTurn = false;
+  isFirstMove = true;
   setTimeout(function() {
     simonNewMove();
   }, 1000);
